80255e6f0512ff41fddb4206c2e4b2ab00eba367
cogito1016 <cogito1016@naver.com>
2022-11-27 16:44:36 +0900

    feat: print

--- basic/01_print.php
+++ basic/01_print.php
@@ -1,10 +1,21 @@
 <?php
 //start PHP Code section
 
+// 큰 따옴표 또ㄴ 작은 따옴표
 echo "Hello";
-echo "<br>";
-echo "My name is";
-echo "KJH";
-
+echo "<br>"; // <br>은 줄바꿈
+echo "My name is ";
+echo 'KJH';
+echo ' haha';
+echo '<br>';
+echo 1234; //숫자만 출력하기위해서는 따옴표 제거
+print '<br>'; //print는 echo와 동일
+print "And...";
+print 5678;
+/*
+주석은 다른언어와 동일
+<!--HTML COMMENT SECTION--> HTML주석처리는 브라우저단의 코드보기로 확인이 가능하므로
+PHP 주서처리를 이용해야 함
+*/
 //end PHP Code Section
 ?>
