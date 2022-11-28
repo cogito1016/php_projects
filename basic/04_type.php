1ec8bd334c0e6dfa19c39ee0857c5415e95b4f1d
cogito1016 <cogito1016@naver.com>
2022-11-28 18:14:45 +0900

    feat: type +

--- basic/04_type.php
+++ basic/04_type.php
@@ -21,4 +21,19 @@ print gettype($null) . "<br>";
 
 $boolean = true;
 print gettype($boolean) . "<br>";
+
+//형변환
+//데이터 형 지정 변환 방법
+$str = "hi";
+$str = (int) $str; // 문자열->숫자 변환이 안되므로 0이 디폴트로 대입됨.
+print "값은 : {$str} 타입은 : " . gettype($str) . "<br>";
+
+$str = "123hi";
+$str = (int) $str; //문자열앞에 숫자가있으면 그건 변환함
+print "값은 : {$str} 타입은 : " . gettype($str) . "<br>";
+
+$str = "hi456";
+$str = (int) $str; //근데 뒤는 변환안함
+print "값은 : {$str} 타입은 : " . gettype($str) . "<br>";
+
 ?>
